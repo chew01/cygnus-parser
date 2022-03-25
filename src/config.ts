8eb16245a814dfc3b72e90a5e8e52a97ca2efc23
chew01 <chewjh19@gmail.com>
2022-03-25 22:40:02 +0800

    Added new fields to limit rankings fetched

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import path from 'path';
+
+config({ path: path.resolve(__dirname, '../.env') });
+
+export const {
+  DB_HOSTNAME, DB_NAME, DB_USERNAME, DB_PASSWORD, API_URL,
+} = process.env;
+export const DB_PORT = Number(process.env.DB_PORT) ?? 5432;
+export const MINIMUM_LEVEL = Number(process.env.MINIMUM_LEVEL) ?? 200;
+
+if (!DB_HOSTNAME) throw new Error('Database hostname was not specified!');
+if (!DB_NAME) throw new Error('Database name was not specified!');
+if (!DB_USERNAME) throw new Error('Database username was not specified!');
+if (!DB_PASSWORD) throw new Error('Database password was not specified!');
+if (!API_URL) throw new Error('API URL was not specified!');
