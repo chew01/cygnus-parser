08e9bcff8344aabd6268620ba7f885ad5f8eba3b
chew01 <chewjh19@gmail.com>
2022-04-02 12:18:23 +0800

    Added exp percentage conversion function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,8 +1,14 @@
 import jobs from '../constants/job';
+import expEarned from '../constants/exp';
 
-function convertJobs(jobId: number, jobDetail: number) {
+export function convertJobs(jobId: number, jobDetail: number) {
   const fullJobId = `${jobId}${jobDetail}`;
   return jobs[fullJobId];
 }
 
-export default convertJobs;
+export function convertExpPercentage(level: number, exp: bigint): number {
+  if (level === 300) return 0;
+  const levelDifference = expEarned[level + 1] - expEarned[level];
+  const percentageBigint = (BigInt(exp) * BigInt(100000)) / levelDifference;
+  return Number(percentageBigint) / 100000;
+}
